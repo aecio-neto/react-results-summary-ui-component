196f3843479026fd5ad3c7bed95d72418472221f
Aécio Neto <aecioneto@MacBook-Pro-de-Aecio-2.local>
2023-08-11 08:49:22 -0300

    create Button component

---
+++ src/components/Button.js
@@ -0,0 +1,7 @@
+const Button = () => {
+  return (
+    <button className='min-w-full p-[0.8em] px-8 mt-4 rounded-full bg-DarkGrayBlue text-White'>Continuar</button>
+  )
+}
+
+export default Button
