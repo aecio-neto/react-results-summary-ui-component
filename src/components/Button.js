65772309fb3158f5677c6f8ed317e6ab710044d1
Aécio Neto <aecioneto@MacBook-Pro-de-Aecio-2.local>
2023-08-14 10:56:08 -0300

    add hover effect

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,16 @@
+import React, { useState } from 'react'
+
 const Button = () => {
+  const [isHovered, setIsHovered] = useState(false)
+
   return (
-    <button className='min-w-full p-[0.8em] px-8 mt-4 rounded-full bg-DarkGrayBlue text-White'>Continuar</button>
+    <button
+      className={`min-w-full p-[0.8em] px-8 mt-4 rounded-full text-White ${isHovered ? 'bg-gradient-to-b from-LightSlateBlue to-LightRoyalBlue' : 'bg-DarkGrayBlue'}`}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      Continuar
+    </button>
   )
 }
 
